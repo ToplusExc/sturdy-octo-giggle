5ae72268a0294dae05a589f9b12db2f8af659358
ToplusExc <ikysyptraaofficial@gmail.com>
2025-09-03 15:50:41 +0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    console.log('Website scripts are loaded and ready.');
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+
+});
